ec23dfe19d3e11fb9edf8ba48418517402e79f74
Marshall T. Rose <mrose17@gmail.com>
2017-04-27 12:21:47 -0700

    0.9.16: remove .js when creating the workers on platform.win32

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http')
 var https = require('https')
 var Joi = require('joi')
 var ledgerPublisher = require('ledger-publisher')
+var os = require('os')
 var path = require('path')
 var random = require('random-lib')
 var underscore = require('underscore')
@@ -926,7 +927,11 @@ Client.prototype.credentialWorker = function (operation, payload, callback) {
 
   self.callbacks[msgno] = { verboseP: self.options.verboseP, callback: callback }
 
-  worker = self.options.createWorker('ledger-client/worker.js')
+  if (os.platform() !== 'win32') {
+    worker = self.options.createWorker('ledger-client/worker.js')
+  } else {
+    worker = self.options.createWorker('ledger-client/worker')
+  }
   worker.onmessage = function (evt) {
     const response = evt.data
     var state = self.callbacks[response.msgno]
